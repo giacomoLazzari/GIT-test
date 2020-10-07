d616c6bced95750e98fb7f9336168c1bc002dba6
Giacomo Lazzari <GIACOMO.LAZZARI@local.ittcesena.it>
2020-10-07 11:07:43 +0200

    first step

---
+++ GIT test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GIT_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] v = new int[10]  { 4, 5, 7,2,9,4,7,44,12,23};
+            int n;
+            for(int i = 0;i< v.Length-1; i ++)
+            {
+                if(v[i]> v[i+1])
+                {
+                    n = v[i];
+                    v[i] = v[i + 1];
+                    v[i + 1] = n;
+                }
+                Console.WriteLine(v[i] + ",");
+            }
+            
+
+        }
+    }
+}

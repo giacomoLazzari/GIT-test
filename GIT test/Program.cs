05a8c806351db79929629f887d02b074d2dcbce5
lorenzo.lizzo <lorenzo.lizzo@PC7312>
2020-10-21 11:20:54 +0200

    Ho inserito il metodo per il calcolo del minimo

--- GIT test/Program.cs
+++ GIT test/Program.cs
@@ -6,19 +6,24 @@ namespace GIT_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] v = new int[10]  { 4, 5, 7,2,9,4,7,44,12,23};
-            ordina(v);
-            for(int i = 0; i< v.Length;i++)
+            int[] v = new int[10] { 4, 5, 7, 2, 9, 4, 7, 44, 12, 23 };
+            Ordina(v);
+            Console.WriteLine("L' array ordinato è: ");
+            for (int i = 0; i < v.Length; i++)
             {
-                Console.WriteLine(v[i] + ",");
+                Console.WriteLine(v[i]);
             }
+            Console.WriteLine("Il minimo tra questi valori è: " + Minimo(v));
 
         }
-
-        static void ordina(int[] v)
+        static void Ordina(int[] v)
         {
             Array.Sort(v);
+        }
+
+        static int Minimo(int[] v)
+        {
+            return v[0];
         }
     }
 }
